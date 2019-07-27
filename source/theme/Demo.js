f45d6326dea5c65ea415d421cfb460a0f7d241fb
Thomas Greco <tgreco1610@gmail.com>
2019-07-27 00:41:04 -0400

    pushing theme demo

--- source/theme/Demo.js
+++ source/theme/Demo.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import { Box, Button } from '@material-ui/core';
-import { makeStyles, createStyles } from '@material-ui/styles';
+import React from 'react'
+import { Box, Button } from '@material-ui/core'
+import { makeStyles, createStyles } from '@material-ui/styles'
 
-const useStyles = makeStyles(({ palette, spacing }) =>
-	createStyles({
-		root: {
-			backgroundColor: palette.primary.light,
-			padding: spacing(2),
-			display: 'flex',
-			height: '100vh',
-			justifyContent: 'space-around'
-		},
-		buttonStyles: {
-			width: '30%',
-			maxHeight: 120,
-			backgroundColor: palette.secondary.dark,
-			'&:hover': {
-				backgroundColor: palette.secondary.light
-			}
+const useStyles = makeStyles(({ palette, spacing }) => ({
+	root: {
+		backgroundColor: palette.primary.light,
+		padding: spacing(2),
+		display: 'flex',
+		height: '100vh',
+		justifyContent: 'space-around'
+	},
+	buttonStyles: {
+		width: '30%',
+		maxHeight: 120,
+		backgroundColor: palette.secondary.dark,
+		'&:hover': {
+			backgroundColor: palette.secondary.light
 		}
-	})
-);
+	}
+}))
 
 const ThemeDemo = () => {
-	const { buttonStyles, root } = useStyles();
+	const { buttonStyles, root } = useStyles()
 	return (
 		<Box className={`${root}`}>
 			<Button variant="contained" className={`${buttonStyles}`}>
@@ -33,7 +31,7 @@ const ThemeDemo = () => {
 				User Sign Up
 			</Button>
 		</Box>
-	);
-};
+	)
+}
 
-export default ThemeDemo;
+export default ThemeDemo
