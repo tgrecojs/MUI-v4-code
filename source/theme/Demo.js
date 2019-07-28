75b01bfd1da95396f1c1872ceb9bfa45a236a954
Thomas Greco <tgreco1610@gmail.com>
2019-07-27 23:24:32 -0400

    added code for intro lesson on mui theming

--- source/theme/Demo.js
+++ source/theme/Demo.js
@@ -1,34 +1,15 @@
 import React from 'react'
 import { Box, Button } from '@material-ui/core'
-import { makeStyles } from '@material-ui/styles'
 
-const useStyles = makeStyles(({ palette: { primary, secondary, common }, spacing }) => ({
-	root: {
-		backgroundColor: primary.light,
-		padding: spacing(2),
-		display: 'flex',
-		height: '100vh',
-		justifyContent: 'space-around'
-	},
-	buttonStyles: {
-		backgroundColor: secondary.dark,
-		color: common.white,
-		maxHeight: 120,
-		width: '30%',
-		'&:hover': {
-			backgroundColor: secondary.light
-		}
-	}
-}))
-
-const ThemeDemo = () => {
-	const { root, buttonStyles } = useStyles()
-	return (
-		<Box className={`${root}`}>
-			<Button className={`${buttonStyles}`}>Login</Button>
-			<Button className={`${buttonStyles}`}>Sign Up</Button>
-		</Box>
-	)
-}
+const ThemeDemo = () => (
+	<Box>
+		<Button variant="contained" color="primary" fullWidth>
+			Login
+		</Button>
+		<Button variant="contained" color="secondary" fullWidth>
+			Sign Up
+		</Button>
+	</Box>
+)
 
 export default ThemeDemo
